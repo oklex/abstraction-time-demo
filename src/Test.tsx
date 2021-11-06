f5427ebf257f171c96f52024ed23fc76f3c7181d
oklex <alexkim.347@gmail.com>
2021-11-06 14:15:39 -0700

    fixed working memory update bugs

--- src/Test.tsx
+++ src/Test.tsx
@@ -84,7 +84,7 @@ export class TestLogic extends React.Component<ITestProps, ITestState> {
   };
 
   runTestStep = async () => {
-    console.log("run test step");
+    console.log("run test step: ", this.state.activeMemory);
     let activeMemoryLength: number = this.state.activeMemory.length;
     if (activeMemoryLength > 0) {
       // find all nodes from this one
@@ -93,7 +93,7 @@ export class TestLogic extends React.Component<ITestProps, ITestState> {
       let prevId: string = mostRecentNode.id;
       let nextNode: IMemoryNode;
       let nextNodeOptions: INode[] = this.getNodeDetails(prevId).linksFrom;
-      console.log("run test step: check node options -", nextNodeOptions);
+      // console.log("run test step: check node options -", nextNodeOptions);
 
       if (nextNodeOptions.length === 0) {
         throw new Error(
@@ -106,25 +106,25 @@ export class TestLogic extends React.Component<ITestProps, ITestState> {
       await Promise.all([
         nextNodeOptions.forEach(async (node) => {
           let optionId = node.to;
-          console.log("run test step: check node for -", optionId);
+          // console.log("run test step: check node for -", optionId);
           let currentIdDiff: number = findDiff(
             this.state.destinationId,
             optionId
           ).length;
-          console.log("run test step: check node one by one -", currentIdDiff);
+          // console.log("run test step: check node one by one -", currentIdDiff);
           if (currentIdDiff < prevIdDiff) {
-            console.log("run test step: found a better match -", optionId);
+            // console.log("run test step: found a better match -", optionId);
             prevIdDiff = currentIdDiff;
             nextNode = {
               id: optionId,
               abstractionPath: this.checkAbstractionPath(optionId),
             };
-            console.log('next node:', nextNode)
+            // console.log('next node:', nextNode)
           }
-        })
-      ]).then(() => {
-        console.log("run test step: update memory -", nextNode);
-        this.updateMemory(nextNode);
+        }),
+      ]).then(async () => {
+        // console.log("run test step: update memory -", nextNode);
+        await this.updateMemory(nextNode);
       });
 
       // call add to memory
@@ -137,28 +137,27 @@ export class TestLogic extends React.Component<ITestProps, ITestState> {
     return [];
   };
 
-  updateMemory = (newItem: IMemoryNode) => {
-    console.log("update memory -", newItem, this.state.activeMemory);
+  updateMemory = async (newItem: IMemoryNode) => {
+    // console.log("update memory -", newItem, this.state.activeMemory);
     this.setState({
-      activeMemory: [...this.state.activeMemory, newItem],
+      activeMemory: this.state.activeMemory.concat([newItem]),
     });
 
     // check and update abstraction capacity
-    let pathLength: number = newItem.abstractionPath
+    let abstractPathLength: number = newItem.abstractionPath
       ? newItem.abstractionPath.length
       : 0;
-    if (pathLength > this.state.abstractionCapacityRemaining) {
+    if (abstractPathLength > this.state.abstractionCapacityRemaining) {
       throw Error("abstraction path is too long");
     } else {
       this.setState({
         abstractionCapacityRemaining:
-          this.state.abstractionCapacityRemaining - pathLength,
+          this.state.abstractionCapacityRemaining - abstractPathLength,
       });
     }
 
     // check and update working memory
-    if (pathLength > this.props.workingMemoryLimit) {
-      let lastIndex: number = pathLength - 1;
+    if (this.state.activeMemory.length > this.props.workingMemoryLimit) {
       let abstractionToRemove: number = 0;
       let nodeToRemove: IMemoryNode = this.state.activeMemory[0];
 
@@ -166,84 +165,93 @@ export class TestLogic extends React.Component<ITestProps, ITestState> {
         abstractionToRemove = nodeToRemove.abstractionPath.length;
       }
       this.setState({
-        activeMemory: this.state.activeMemory.slice(1, lastIndex), // pop first item
+        activeMemory: this.state.activeMemory.slice(1), // pop first item
         abstractionCapacityRemaining:
           this.state.abstractionCapacityRemaining - abstractionToRemove,
       });
     }
-    console.log("update memory -", newItem, this.state.activeMemory);
+
+    // await this.updateColor()
+    // console.log("update memory -", newItem, this.state.activeMemory);
   };
 
   generateDestination = (
     destinationDepth: number = this.props.depth,
     maxChildNodes: number = this.props.maxChildNodes
   ) => {
-  //   console.log("set destination with: ", destinationDepth, maxChildNodes);
-  //   let max: String | Number = "";
-  //   for (let i = 0; i < destinationDepth; i++) {
-  //     max = String(max) + String((maxChildNodes > 9)? 9 : maxChildNodes);
-  //   }
-  //   let combination = String(getRandomInt(0, Number(max)));
-  //   // process number to remove illegal digits
-  //   for (let i = 0; i < combination.length; i++) {
-  //     console.log("checking each digit: ", combination[i]);
-  //     if (Number(combination[i]) > maxChildNodes) {
-  //       let preppendCombination: string =
-  //         i - 1 >= 0 ? combination.slice(0, i - 1) : "";
-  //       let appendCombination: string =
-  //         i + 1 <= combination.length
-  //           ? combination.slice(i + 1, combination.length)
-  //           : "";
-  //       combination =
-  //         preppendCombination + String(maxChildNodes - 1) + appendCombination;
-  //     }
-  //   }
+    //   console.log("set destination with: ", destinationDepth, maxChildNodes);
+    //   let max: String | Number = "";
+    //   for (let i = 0; i < destinationDepth; i++) {
+    //     max = String(max) + String((maxChildNodes > 9)? 9 : maxChildNodes);
+    //   }
+    //   let combination = String(getRandomInt(0, Number(max)));
+    //   // process number to remove illegal digits
+    //   for (let i = 0; i < combination.length; i++) {
+    //     console.log("checking each digit: ", combination[i]);
+    //     if (Number(combination[i]) > maxChildNodes) {
+    //       let preppendCombination: string =
+    //         i - 1 >= 0 ? combination.slice(0, i - 1) : "";
+    //       let appendCombination: string =
+    //         i + 1 <= combination.length
+    //           ? combination.slice(i + 1, combination.length)
+    //           : "";
+    //       combination =
+    //         preppendCombination + String(maxChildNodes - 1) + appendCombination;
+    //     }
+    //   }
     this.setState({
-      destinationId: '0000000000',
-    })
-  //   console.log("set destination as: ", combination);
-  //   return combination;
+      destinationId: "0000000000",
+    });
+    //   console.log("set destination as: ", combination);
+    //   return combination;
   };
 
   checkDestination = () => {
     const memoryLength: number = this.state.activeMemory.length;
-    console.log(
-      "checkDestination: ",
-      this.state.activeMemory[memoryLength - 1],
-      this.state.destinationId
-    );
     if (
       memoryLength > 1 &&
       this.state.destinationId &&
       this.state.activeMemory[memoryLength - 1].id === this.state.destinationId
     ) {
-      console.log('at destionation')
+      console.log("at destionation");
       return true;
     } else {
-      console.log('not at destionation')
+      // console.log('not at destionation')
       return false;
     }
   };
 
-  updateColor = async (e: any) => {
-    let nodes = this.mychart.chart.series[0].nodes;
+  updateColor = async () => {
+    let allNodes = this.mychart.chart.series[0].nodes;
     // find the node with the right id
-    await nodes.forEach((node: any, index: number) => {
-      if (node.id === e.point.id) {
-        this.mychart.chart.series[0].nodes[index].update({
-          color: "#aaa",
-        });
-      }
+    await allNodes.forEach((node: any, index: number) => {
+      this.state.activeMemory.map((memoryNode) => {
+        if (node.id === memoryNode.id) {
+          this.mychart.chart.series[0].nodes[index].update({
+            color: "#aaa",
+          });
+        }
+      });
     });
   };
 
   getNodeDetails = (nodeId: string) => {
-    let nodes = this.mychart.chart.series[0].nodes;
+    console.log('get node details')
+    let nodes: INode[] = this.mychart.chart.series[0].nodes;
+    let returnNode: any | null = null;
+    console.log('nodes: ', nodes)
     for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i].id === nodeId) {
-        return nodes[i];
+      console.log('get node details: ', nodes[i], nodeId)
+      if (String(nodes[i].id) === String(nodeId)) {
+        returnNode = nodes[i];
       }
     }
+     if (!returnNode) {
+       throw new Error("no return node was found in list:verify that this node id is correct")
+     } else {
+       console.log('node details found:', returnNode)
+     return returnNode;
+     }
   };
 
   render() {
